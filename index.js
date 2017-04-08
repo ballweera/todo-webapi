f759b324c747b1bc1d2dab31043f780a4824ca1f
Weera Kasetsin <ballweera@daikuren-hyorinmaru.local>
2017-04-08 10:22:39 +0700

    Create API: POST /todos

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+    let newTodo = req.body
+    let todos = []
+
+    newTodo.id = 1
+    todos.push(newTodo)
+
+    res.status(201).json({ message: 'Todo is created', data: newTodo })
+})
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Express App' })

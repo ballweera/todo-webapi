e8c07cbfa2f4164916d12f88f5987e37316f0a97
Weera Kasetsin <ballweera@daikuren-hyorinmaru.local>
2017-04-08 11:53:15 +0700

    Complete API v1.0

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
+
+let todos = [
+    { id: 1, title: 'Learn NodeJS', completed: false },
+    { id: 2, title: 'Create Express App', completed: true }
+]
+
+app.get('/todos', (req, res) => {
+    res.json(todos)
+})
 
 app.post('/todos', (req, res) => {
     let newTodo = req.body
-    let todos = []
 
-    newTodo.id = 1
+    newTodo.id = todos.length + 1
     todos.push(newTodo)
 
-    res.status(201).json({ message: 'Todo is created', data: newTodo })
+    res.status(201).json(newTodo)
 })
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello Express App 222' })
 })
 
-app.listen(3000, () => {
-    console.info('Server started at port 3000')
+app.listen(PORT, () => {
+    console.info(`Server started at port ${PORT}`)
 })
